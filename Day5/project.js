0909f90a307fd3f7b3c8e8db449567dee2859193
mujet <zain.murtafi@gmail.com>
2022-08-15 09:28:36 +0700

    restyle project form, add enter in mailto body

--- Day5/project.js
+++ Day5/project.js
@@ -20,8 +20,7 @@ function submitData() {
 
     let a = document.createElement('a')
 
-    emailBody = `\r\nmy name is ${name}. ${message} \r\n\r\nSincerely, \r\n${name} \r\n${email} | ${phone}`
-    emailBody = encodeURIComponent(emailBody)
+    emailBody = `%0D%0Amy name is ${name}. ${message} %0D%0ASincerely, %0D%0A${name} %0D%0A${email} | ${phone}`
 
     a.href = `mailto:zain.murtafi@gmail.com?subject=${subject}&body=Greeting! ${emailBody}`
     a.click()
